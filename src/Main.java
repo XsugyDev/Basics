e585514e1866a53064fc1608e525229d2e90dd10
edwin <xsugy.dev@gmail.com>
2025-09-24 00:08:55 +0100

    1.02

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         char Java = 'H';
+        String Code = "Hola";
         System.out.println(Java);
+        System.out.println(Code);
     }
 }
