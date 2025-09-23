b04503b3ef7362e2b0c187ee0f16b66b576675d3
edwin <xsugy.dev@gmail.com>
2025-09-24 00:23:07 +0100

    1.02 Fix

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         char Java = 'H';
         String Code = "Hola";
+        int Paper = 1;
         System.out.println(Java);
         System.out.println(Code);
+        System.out.print(Paper);
     }
 }
